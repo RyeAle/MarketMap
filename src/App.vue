5f2b72b92cfe5fb16a4ba7e5f164c4a0f714dde7
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-05-25 17:46:51 +0300

    Gleb: created home page with basic ymap

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app>
+    <v-content fill class="pt-0">
+        <router-view/>
+    </v-content>
+    </v-app>
   </div>
 </template>
+<script>
+  import Toolbar from './components/Toolbar.vue';
 
+  export default {
+    components: { Toolbar },
+  }
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-toolbar app class="pt-2">
+    <v-text-field
+      outline
+      label="Выберите магазин"
+      append-icon="place"
+    ></v-text-field>
+  </v-toolbar>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
