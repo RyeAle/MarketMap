9824f6798ef1cc64f70a6b12265ab307c23ef9ea
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-05-25 13:07:27 +0300

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'main', 'resources', 'public', 'js', 'main.js'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        loader: ['style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src', 'main', 'resources', 'public', 'js'),
+      path.join(__dirname, 'src', 'main', 'resources', 'public', 'img'),
+      path.join(__dirname, 'node_modules'),
+    ],
+  }
+};
