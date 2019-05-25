71b9fecdcf99631b937b059977809d46ed905521
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-05-25 15:59:31 +0300

    Gleb: updated main and router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import '@babel/polyfill'
+import store from 'store/store'
+import 'api/resourse'
 import router from './router'
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
