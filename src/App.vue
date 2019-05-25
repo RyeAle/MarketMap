d42c988f6684ca05be9796ac6f2a2f16171e7c68
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-05-25 23:23:53 +0300

    Gleb: added home page toggl button and bottom nav

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,21 @@
     <v-content fill class="pt-0">
         <router-view/>
     </v-content>
-        <toolbar style="z-index: 100; position: absolute; width: 90%;"/>
-        <navigation-drawer />
+
     </v-app>
   </div>
 </template>
 <script>
-  import Toolbar from './components/Toolbar.vue';
-  import NavigationDrawer from './components/NavigationDrawer.vue';
 
   export default {
-    components: { Toolbar, NavigationDrawer },
+    methods: {
+      geoRequest: () => {
+        navigator.permissions.query({name:'geolocation'}).then(function(result) {});
+      }
+    },
+    mounted() {
+      this.geoRequest()
+    }
   }
 </script>
 <style>
---
+++ src/components/ShopList.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout
+    wrap
+    height="100%"
+  >
+    <v-btn
+      absolute
+      style="left:50%; bottom: 0%; transform: translate(-50%); width: 100%; height: 5%"
+      @click.stop="showNav = !showNav"
+    >
+      <v-icon dark>list</v-icon>
+    </v-btn>
+
+    <v-bottom-nav
+      app
+      :active.sync="activeBtn"
+      :value="showNav"
+      absolute
+    >
+      <v-btn flat color="teal">
+        <span>Recents</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        activeBtn: 1,
+        showNav: false
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
