b62f59bb975583f6491c847cda2bb29edd0d5719
Nick <nickkarmatskikh@gmail.com>
2019-05-25 13:43:25 +0300

    init

---
+++ src/main/java/ru/gatchina/marketmap/MarketMapApplication.java
@@ -0,0 +1,13 @@
+package ru.gatchina.marketmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketMapApplication.class, args);
+    }
+
+}
