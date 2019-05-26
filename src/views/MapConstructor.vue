52a430cb0c803fad882d42a02b1528bfc0c4e90e
dmitriyace <chistohodovda@gmail.com>
2019-05-26 09:51:30 +0300

    Dima: refactoring 70%

--- src/views/MapConstructor.vue
+++ src/views/MapConstructor.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h4>{{stages[currentStage].header}}</h4>
-    <div class="content"></div>
+    <div id="svgmap" class="content"></div>
     <!--<v-dialog v-model="dialog" width="90%"/>-->
     <button
       @click="goNextStep"
@@ -9,6 +9,7 @@
       Go next
     </button>
     <button
+      v-if="currentStage !== 0"
       @click="goPrevStep"
       style="position: absolute; bottom: 0; left: 20px; background: #666; width: 100px; height: 50px;">
       Go prev
@@ -22,6 +23,10 @@
   import axios from 'axios';
 
   export default {
+    props: {
+      mapWidth: Number,
+      mapHeight: Number,
+    },
     data: () => ({
       currentStage: 0,
       showCategoryPopup: false,
@@ -71,10 +76,11 @@
               this.showCategoryPopup = true;
               break;
             case 3:
-              if (element.tagName !== 'image')
+              if (element.tagName !== 'image') {
                 replaceElement(element);
-              else
-              break;
+              } else {
+                break;
+              }
             case 5:
               this.showProductsPopup = true;
               break;
@@ -88,19 +94,70 @@
       replaceElementToImage(element) {
         const image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
         image.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', './door.png');
-        },
+      },
       replaceElementToRect(element) {
 
       },
+      sendMap() {
+        let blocks = document.getElementsByClassName('clickable');
+        let map = [];
+        Array.from(blocks)
+          .forEach((val, i) => {
+            let idArray = val.id.split(':');
+            if (val.getAttribute('category') != undefined) {
+              map[i] = {
+                x: idArray[1],
+                y: idArray[2],
+                blockType: val.category,
+                category: {}
+              };
+            }
+          });
+        const data = {
+          latitude: Math.random(),
+          longitude: Math.random(),
+          width: document.getElementById('svg').viewBox.baseVal.width / 100,
+          height: document.getElementById('svg').viewBox.baseVal.height / 100,
+          floor: 1,
+          blocks: map.filter(it => it.x != null)
+        };
+        axios.post('http://192.168.42.55:8080/map/add', data)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        console.log(json);
+      },
       goNextStep() {
-
+        if (this.currentStage + 1 > this.stages.length - 1) {
+          this.currentStage++;
+        } else {
+          this.sendMap();
+          this.$router.push('/');
+        }
       },
       goPrevStep() {
-
+        if (this.currentStage !== 0) {
+          this.currentStage--;
+        }
       },
       init() {
 
-      }
+      },
+    },
+    mounted() {
+      let angleScale = {
+        angle: 0,
+        scale: 1,
+      };
+      const scalable = createGrid(this.mapWidth, this.mapHeight);
+      scalable.id = 'svg';
+      const container = document.getElementById('svgmap');
+      container.append(scalable);
+      const obj = document.getElementsByClassName('clickable');
+      
     }
   };
 
