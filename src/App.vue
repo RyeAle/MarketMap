2c11f76b80afec2a3c215210b426b6c139c661c4
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-05-26 04:54:09 +0300

    zhopa

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <v-app>
-    <v-content fill class="pt-0">
+      <v-content fill class="pt-0">
         <router-view/>
-    </v-content>
-
+      </v-content>
     </v-app>
   </div>
 </template>
@@ -12,21 +11,39 @@
 
   export default {
     methods: {
-      geoRequest: () => {
-        navigator.permissions.query({name:'geolocation'}).then(function(result) {});
+      geoRequest() {
+        // navigator.permissions.query({name: 'geolocation'}).then(function (result) {
+        //   alert(result.state);
+        // });
+        navigator.geolocation.getCurrentPosition(
+          function(position)
+          {
+            console.log("Доступ разрешён.");
+
+            //действия с полученными данными
+          },
+          function(error)
+          {
+            // если ошибка (можно проверить код ошибки)
+            if(error.PERMISSION_DENIED)
+            {
+              console.log("В доступе отказано!");
+              alert("fsdfdsfsfd");
+            }
+          });
       }
     },
     mounted() {
-      this.geoRequest()
+      //this.geoRequest()
     }
   }
 </script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
--- src/components/ListRow.vue
+++ src/components/ListRow.vue
@@ -1,53 +1,52 @@
 <template>
-    <v-container grid-list-md>
-        <v-layout row>
-            <v-flex xs8 offset-xs4 class="header">
-                {{title}} <span style="font-size: 0.85em">{{distance}}</span> 
+  <v-flex class="my-1 text-xs-left">
+    <v-layout row>
+      <v-flex xs3>
+        <v-img :src="'https://lh6.googleusercontent.com/-l0_KVe2-FaE/AAAAAAAAAAI/AAAAAAAAcbo/_6Y8NSP3pOg/photo.jpg'" class="ml-2 my-2"></v-img>
+      </v-flex>
+      <v-flex xs9 class="pl-4">
+          <v-layout column align-space-around>
+            <v-flex class="mt-2">
+              <span class="text-uppercase font-weight-bold mr-1">{{this.shop.network.name}}</span>
+              <span style="font-size: 0.85em">({{this.distance}})</span>
             </v-flex>
-        </v-layout>
-        <v-layout row>
-            <v-flex xs8 offset-xs4 class="address">
-                {{address}}
+            <v-flex>
+              {{this.address}}
             </v-flex>
-        </v-layout>
-        <v-layout row>
-            <v-flex xs8 offset-xs4 class="date">
-                {{openTime}}
+            <v-flex>
+              {{this.shop.openTime}}-{{this.shop.closeTime}}
             </v-flex>
-        </v-layout>
-        <img :src="logoUrl" />
-    </v-container>
+          </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-flex>
 </template>
 
 <script>
-    export default {
-        props:['title', 'distance', 'address', 'openTime', 'logoUrl' ]
+  import axios from 'axios'
+  export default {
+    data:() => ({
+      address: null,
+      distance: 150
+    }),
+    props: ['shop'],
+    created() {
+        axios({
+          method: 'get',
+          url: 'https://geocode-maps.yandex.ru/1.x/?apikey=' +
+            'c05046c2-3599-474b-a74d-dac5c5e3e0d5' + '&format=json&geocode=' +
+            this.shop.longitude + ',' + this.shop.latitude,
+        }).then(response => {
+          this.address = response.data.response.GeoObjectCollection.featureMember[0].GeoObject.name;
+          console.log(response.data);
+        }).catch(function (error) {
+          console.log(error);
+        }).finally(function () {
+        });
     }
+  }
 </script>
 
 <style scoped>
-.header{
-    margin-left: 71px !important;
-    margin-top: 10px;
-    font-weight: bold;
-    font-size: 0.85em;
-}
-
-.address {
-    margin-top: -10px;
-    margin-left: 77px !important;
-}
-
-.date {
-    margin-top: -10px;
-    margin-left: 52px !important;
-}
-
-img{
-    position: absolute;
-    top: 3px;
-    left: 30px;
-    width: 22%;
-}
 </style>
 
